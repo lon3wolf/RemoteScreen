d3a19736cdf76cf817556689e930c15fd6880330
sanil_sun_cp <SND\sanil_sun_cp>
2009-03-05 22:52:14 +0000

    XRS Version 2.0 Beta server only * Provides additional HTTP interface (Browser as a client)

---
+++ RemoteScreen2/Common.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Drawing;
+namespace RemoteScreen
+{
+    public static class Common
+    {
+        public const string FilePath = @"C:\sd.jpg";
+
+        public static void SaveScreenImage()  //Saves Desktop Image to C:\sd.jpg
+        {
+            Bitmap bmp = CaptureScreen.GetDesktopImage();
+            bmp.Save(FilePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        public static byte[] GetLatestImage() //Gets the saved desktop image byte buffer from pre defined source C:\sd.jpg
+        {
+            byte[] buf = File.ReadAllBytes(FilePath);
+
+            File.Delete(FilePath);
+
+            return buf;
+        }
+
+    }
+}
